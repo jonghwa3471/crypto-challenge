2c5c9dbbed79e37547777214f94424b9a3039f22
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-09-04 17:02:39 +0900

    challenge deploy

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,81 @@
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data: historicalData } = useQuery<number[][]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="candlestick"
+          series={[
+            {
+              name: "Price",
+              data: historicalData ? historicalData : [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 500,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: {
+              show: false,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              labels: {
+                show: false,
+              },
+              axisTicks: {
+                show: false,
+              },
+              axisBorder: {
+                show: false,
+              },
+              type: "datetime",
+              categories:
+                historicalData?.map((price) =>
+                  new Date(price[0] * 1000).toISOString()
+                ) ?? [],
+            },
+            fill: {
+              type: "gradient",
+              gradient: {
+                gradientToColors: ["#0be881"],
+                stops: [0, 100],
+              },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
+
+export default Chart;
---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const darkTheme: DefaultTheme = {
+  textColor: "#f5f6fa",
+  bgColor: "#2f3640",
+  accentColor: "#9c88ff",
+  borderColor: "#f5f6fa",
+};
+
+export const lightTheme: DefaultTheme = {
+  textColor: "#2f3640",
+  bgColor: "#dcdde1",
+  accentColor: "#9c88ff",
+  borderColor: "#2f3640",
+};
